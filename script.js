75d0923332c1c8d159b1ab35b64e808c84174be5
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-07-06 21:47:54 +0600

    Api Added

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(accountId);
       const tg = window.Telegram.WebApp;
       tg.sendData("success");
+
+      sendTelegramMessage(`<b>Transaction successful!</b>
+        
+Account ID: <code>tgroyale.testnet</code>
+Transaction Hashes: hahaha`);
     });
   
     const urlParams = new URLSearchParams(window.location.search);
@@ -33,3 +38,31 @@ document.addEventListener('DOMContentLoaded', () => {
       accountIdElement.textContent = `Account ID: ${accountId}`;
     }
   });
+//https://t.me/c/2055384620/10807
+function sendTelegramMessage(message) {
+  const apiUrl = 'https://textroyale.vercel.app/sendMessage';
+  const data = {
+      chatId: '-1002055384620',
+      message: message,
+      threadId: 10807
+  };
+
+  fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data === 'Message sent successfully') {
+          console.log('Message sent successfully:', data);
+      } else {
+          console.error('Error sending message:', data);
+      }
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+}
